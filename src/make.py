e2471d1f406938221cd672d6dad8bdc8c0d76a6a
Your Name <you@example.com>
2023-03-01 13:29:38 +0800

    fix(make): fix unable to move executable

--- src/make.py
+++ src/make.py
@@ -2,17 +2,20 @@ import os
 import sys
 import subprocess
 import shutil
+import time
 
 from definitions import DIR_ROOT, DIR_PIC
 
+name = str(int(time.time()))
 
 subprocess.run(
     ["pyinstaller", "-F", "-w", "--uac-admin",
      os.path.join(DIR_ROOT, "src", "main.py"),
-     "--icon=" + os.path.join(DIR_PIC, "icon.ico")]
+     "--icon=" + os.path.join(DIR_PIC, "icon.ico"),
+     f"-n{name}"]
 )
 
-shutil.move(os.path.join(DIR_ROOT, "dist", "main.exe"), DIR_ROOT)
-os.remove(os.path.join(DIR_ROOT, "main.spec"))
+shutil.move(os.path.join(DIR_ROOT, "dist", f"{name}.exe"), DIR_ROOT)
+os.remove(os.path.join(DIR_ROOT, f"{name}.spec"))
 shutil.rmtree(os.path.join(DIR_ROOT, "build"))
 shutil.rmtree(os.path.join(DIR_ROOT, "dist"))
