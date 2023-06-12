cb667559cd87f233f0b7790e250142af4463c549
SuperDumbTM <eltonlochunkit@gmail.com>
2023-06-12 17:24:45 +0800

    feat: set password for current user

---
+++ src/utils/commands.py
@@ -0,0 +1,38 @@
+import sys
+from subprocess import Popen, check_output
+from typing import Optional, Union
+import wmi
+
+
+_WMI = wmi.WMI()
+
+
+def shutdown(timeout: Union[int, float] = 1):
+    return Popen(["shutdown", "/s", "/t", timeout], stdout=sys.stdout)
+
+
+def reboot(timeout: Union[int, float] = 1):
+    return Popen(["shutdown", "/r", "/t", timeout], stdout=sys.stdout)
+
+
+def set_password(username: str, password: Optional[str]):
+    if password is not str or password is not None:
+        raise TypeError("Password only accepts string or None")
+    elif len(password) > 0:
+        return Popen(
+            ["powershell.exe", "Set-LocalUser", "-Name", username, "-Password",
+                f"(ConverTo-SecureString {password} -AsPlainText -Force)"],
+            stdout=sys.stdout)
+    else:
+        return Popen(
+            ["powershell.exe", "Set-LocalUser", "-Name", username,
+                "-Password", "(new-object System.Security.SecureString)"],
+            stdout=sys.stdout)
+
+
+def get_current_usrname() -> str:
+    return check_output(["powershell.exe", "$Env:UserName"]).strip().decode()
+
+
+def get_users_info() -> list[wmi._wmi_object]:
+    return _WMI.Win32_UserAccount()
--- src/window_main.py
+++ src/window_main.py
@@ -1,12 +1,12 @@
 import os
 import threading
 from subprocess import Popen
-from typing import Iterator
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 import definitions
 from ui.main import Ui_MainWindow
+from utils import commands
 from widgets.driver_checkbox import DriverOptionCheckBox
 from hw_info_worker import HwInfoWorker
 from window_progress import ProgressWindow
@@ -33,7 +33,7 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
                                           self.qsig_hwinfo,
                                           parent=self)
         self.refresh_hwinfo()
-        # ----- driver options -----
+        # ---------- driver options ----------
         for option in self.driconfg.get_type("network"):
             self.lan_driver_dropdown.addItem(option.name, option.id)
         # check autoable
@@ -57,6 +57,9 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         self.edit_driver_action.triggered.connect(self.dri_conf_window.show)
         self.at_install_cb.clicked.connect(lambda val:self.set_at_options(val))
         self.dri_opt_reset_btn.clicked.connect(self._dri_opt_reset)
+        self.set_passwd_cb.clicked.connect(
+            lambda: self.set_passwd_txt.setEnabled(self.set_passwd_cb.isChecked())
+        )
         # ---------- signals ----------
         self.qsig_msg.connect(self.send_msg)
         self.qsig_hwinfo.connect(
@@ -68,7 +71,7 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         super().closeEvent(event)
     
     def send_msg(self, text: str):
-        """display a message to the UI (message box)
+        """Display a message to the UI (message box)
 
         Args:
             text (str): message to be displayed
@@ -78,7 +81,7 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
             self.prog_msg_box.verticalScrollBar().maximum())  # scroll to bottom
     
     def refresh_hwinfo(self):
-        """rescan and display the hardware information of the computer"""
+        """Rescan and display the hardware information of the computer"""
         for i in reversed(range(self.hwinfo_vbox.count())):
             self.hwinfo_vbox.itemAt(i).widget().setParent(None)
         self.hwinfo_worker.start()
@@ -87,7 +90,7 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         pass
     
     def install(self):
-        """start the install process"""
+        """Start the install process"""
         manager = InstallManager(self.qsig_msg, self.progr_window.qsig_progress)
         manager.qsig_successful.connect(self._post_install)
         
@@ -103,6 +106,13 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         for dri_conf in self.get_selected_dri():
             self.progr_window.append_progress(dri_conf, "等待安裝中")
             manager.add_task(Task(dri_conf))
+        
+        # set password
+        # if self.set_passwd_cb.isChecked():
+        #     commands.set_password(
+        #         commands.get_current_usrname,
+        #         self.set_passwd_txt.toPlainText())
+
         # start install
         if len(manager) == 0:
             box = QtWidgets.QMessageBox()
@@ -148,21 +158,21 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         return dri
     
     def _post_install(self, success: bool):
-        """follow-up routine for the installation process
+        """Follow-up routine for the installation process
 
         Args:
             success (bool): whether the all drivers were installed successfully
         """
-        if not success:
+        if not success and not self.at_retry_cb.isChecked():
             pass
         elif ((self.at_halt_rb.isCheckable() or self.at_halt_rb.isEnabled())
               and self.at_halt_rb.isChecked()):
-            t = threading.Timer(5, lambda: Popen(["shutdown", "/s", "/t", "1"]))
+            t = threading.Timer(5, lambda: commands.shutdown())
             t.start()
             QtWidgets.QMessageBox.information(self, "完成", "安裝成功，即將自動關機")
         elif ((self.at_reboot_rb.isCheckable() or self.at_reboot_rb.isEnabled())
               and self.at_reboot_rb.isChecked()):
-            t = threading.Timer(5, lambda: Popen(["shutdown", "/r", "/t", "1"]))
+            t = threading.Timer(5, lambda: commands.reboot())
             t.start()
             QtWidgets.QMessageBox.information(self, "完成", "安裝成功，即將自動重新開機")
         else:
@@ -182,10 +192,10 @@ class MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
                 QtWidgets.qApp.exit(0)
     
     def _dri_on_select(self):
-        """analyse and update execution options for auto installation mode
-        based on user selection on to be installed drivers
+        """Analyse and update execution options for auto installation mode
+        based on user selection on to be installed drivers.
         
-        if selected drivers contains non-autoable drivers,\
+        E.g. If selected drivers contains non-autoable drivers,\
             disable execution options for auto installation mode
         """
         autoable = all([dri.autoable for dri in self.get_selected_dri()])
