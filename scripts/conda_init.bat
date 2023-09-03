be20a4cc72e269e8c0aba99490dee0ad997f9569
SuperDumbTM <eltonlochunkit@gmail.com>
2023-09-03 22:24:36 +0800

    fix conda init script

--- scripts/conda_init.bat
+++ scripts/conda_init.bat
@@ -1,4 +1,19 @@
-conda create --name dri-installer python=3.11 -y
-conda activate dri-installer
-conda install pywin32 -y
+@echo off
+set ENVNAME="dri-installer"
+
+echo Creating conda enviroment %ENVNAME% with Python version 3.11
+echo:
+call conda create --name %ENVNAME% python=3.11 -y
+timeout 2
+
+echo Installing 'pywin32'.
+echo:
+call conda activate %ENVNAME% && call conda install pywin32 -y
+timeout 2
+
+
+echo Installing required Python packages.
+echo:
+call pip install -r ..\requirements-dev.txt
+
 pause
