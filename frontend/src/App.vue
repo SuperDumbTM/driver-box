e455176997417f174a3cbadd94faff4530df706b
SuperDumbTM <eltonlochunkit@gmail.com>
2024-12-05 12:48:41 +0800

    Use loop to display router links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import FileExeIcon from '@/components/icons/FileExeIcon.vue'
-import GearIcon from '@/components/icons/GearIcon.vue'
-import HomeIcon from '@/components/icons/HomeIcon.vue'
 import * as app_manager from '@/wailsjs/go/store/AppSettingManager'
-import { onBeforeMount } from 'vue'
+import { onBeforeMount, type Component } from 'vue'
 import { useI18n } from 'vue-i18n'
+import type { RouteLocationRaw } from 'vue-router'
+import FileExeIcon from './components/icons/FileExeIcon.vue'
+import GearIcon from './components/icons/GearIcon.vue'
+import HomeIcon from './components/icons/HomeIcon.vue'
 
 const { locale } = useI18n()
+
+const routes: Array<{ to: RouteLocationRaw; icon: Component }> = [
+  { to: '/', icon: HomeIcon },
+  { to: '/drivers', icon: FileExeIcon },
+  { to: '/settings', icon: GearIcon }
+]
 
 onBeforeMount(() => {
   app_manager.Read().then(s => (locale.value = s.language))
@@ -18,31 +25,13 @@ onBeforeMount(() => {
     <aside class="w-12">
       <div class="flex justify-center h-full px-1 py-3 overflow-y-auto bg-gray-50">
         <ul class="pt-3 space-y-3 font-medium">
-          <li>
+          <li v-for="(link, i) in routes" :key="i">
             <RouterLink
-              to="/"
+              :to="link.to"
               class="flex items-center p-2 rounded-lg hover:bg-gray-200"
               activeClass="text-apple-green-900 bg-powder-blue-400"
             >
-              <HomeIcon></HomeIcon>
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink
-              to="/drivers"
-              class="flex items-center p-2 rounded-lg hover:bg-gray-200"
-              activeClass="text-apple-green-900 bg-powder-blue-400"
-            >
-              <FileExeIcon></FileExeIcon>
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink
-              to="/settings"
-              class="flex items-center p-2 rounded-lg hover:bg-gray-200"
-              activeClass="text-apple-green-900 bg-powder-blue-400"
-            >
-              <GearIcon></GearIcon>
+              <component :is="link.icon"></component>
             </RouterLink>
           </li>
         </ul>
